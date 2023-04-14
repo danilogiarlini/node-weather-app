92256ddb68eb772b5fd32235af69fd5ffc7e3a5e
Danilo Giarlini <danilogiarlini@gmail.com>
2023-04-14 15:59:13 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    if(!latitude || !longitude){
+        callback('Provide a valid address.', undefined)
+    }
+
+    const url = 'http://api.weatherstack.com/current?access_key=35bce6f4530f2dec6736cf512875224d&query=' + latitude + ',' + longitude + '&units=m'
+    
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+
+module.exports = forecast
